464921089ee01a71cc55a2d5a7a1afdddaa60805
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-06-14 19:30:02 +0530

    Reordered for a better structure

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,15 @@
     openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDUWMpLWsqaHAvHdvAtTQjF3bMOpnreZP7vKztgWq2MUwNO6QEY4FsD7Uq8lAeDHzUF2xLfO5s2S5n/fUQJeECCWwCQS8HBRUVw+36qQhxFriWoxh7iEQ+NExesX13qX+jQGGebleU5A45fWVkWFfTBtu+8p764Vql8sG0tt08gzxBOcqTwB4h9BSC1AEYgP883OeHcdeCXvtmzTGqA+Kuy8HqT/KTDUtetDA44LH2hKMcJQ4OinibL01fi4Q3UyhEJDIE6j4BawZgiEoF1JAJuj3CT3120wWG3PxGgDf8MSUWbGiubiS0vpspGK1ezuJJydKAfnIclptM2koiSxkGl abdulsattar@abdulsattar-desktop" ];
   };
 
+  security.sudo.extraConfig =
+  ''
+    bimorphic   ALL=(ALL) NOPASSWD:ALL, SETENV: ALL
+  '';
+
+  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+  networking.firewall.allowPing = true;
+  networking.hostName = "bimorphic";
+
   services.openssh = {
     enable = true;
     permitRootLogin = pkgs.lib.mkForce "no";
@@ -25,15 +34,6 @@
     enable = true;
     package = pkgs.postgresql94;
   };
-
-  security.sudo.extraConfig =
-  ''
-    bimorphic   ALL=(ALL) NOPASSWD:ALL, SETENV: ALL
-  '';
-
-  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
-  networking.firewall.allowPing = true;
-  networking.hostName = "bimorphic";
 
   nix.gc.automatic = true;
 }
